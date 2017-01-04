e0d73226478d9efeb634a3dacdd21e8c43a15524
Unknown <sophie.candel@etu.unistra.fr>
2017-01-04 12:29:42 +0100

    ajout page "retrait d'une image en ligne"

---
+++ pages/retrait-image-en-ligne.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <!--HEADER-->
+    <?php include("../includes/header.php"); ?>
+    <head>
+        <meta name="description" content="Informations concernant le droit de retrait d'une image en ligne" />
+        <title>Droit à l'image - droit de retrait d'une image en ligne</title>
+    </head>
+    
+    <body class="texte_fonce">
+        <!--NAV-->
+        <?php include("../includes/nav.php"); ?>
+
+
+        <!--MAIN-->
+        <main>
+           <section>
+              <article>
+                   <h1>Retrait d'une image en ligne</h1>
+                   <h2>Chapô</h2>
+               </article>
+            </section>
+            
+        </main>
+
+
+        <!--FOOTER-->
+        <?php include("../includes/footer.php"); ?>
+    </body>
+</html>
