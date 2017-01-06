fc4f085117cb9b2122ff31dedc47df969613a976
Unknown <sophie.candel@etu.unistra.fr>
2017-01-06 14:50:39 +0100

    tarte au citron

--- includes/header.php
+++ includes/header.php
@@ -6,4 +6,25 @@
     <link rel="stylesheet" href="../styles/reset.css">
     <link rel="stylesheet" href="../styles/font-awesome-4.6.3/css/font-awesome.css">
     <link rel="stylesheet" href="../styles/style.css">
+    
+    <script type="text/javascript" src="../tarteaucitron/tarteaucitron.js"></script>
+
+    <script type="text/javascript">
+        tarteaucitron.init({
+            "hashtag": "#tarteaucitron", /* Ouverture automatique du panel avec le hashtag */
+            "highPrivacy": false, /* désactiver le consentement implicite (en naviguant) ? */
+            "orientation": "top", /* le bandeau doit être en haut (top) ou en bas (bottom) ? */
+            "adblocker": false, /* Afficher un message si un adblocker est détecté */
+            "showAlertSmall": true, /* afficher le petit bandeau en bas à droite ? */
+            "cookieslist": true, /* Afficher la liste des cookies installés ? */
+            "removeCredit": false /* supprimer le lien vers la source ? */
+        });
+    </script>
+    
+     <script type="text/javascript">
+        tarteaucitron.user.analyticsUa = 'UA-88358182-1';
+        tarteaucitron.user.analyticsMore = function () { /* add here your optionnal ga.push() */ };
+        (tarteaucitron.job = tarteaucitron.job || []).push('analytics');
+    </script>
+    
 </head>
