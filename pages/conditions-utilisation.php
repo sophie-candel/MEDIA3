d57a15a5eda13de29ab682f07db3092bd87fa341
Talinona1 <timothe.golla@etu.unistra.fr>
2017-01-08 16:15:32 +0100

    CGU

--- pages/conditions-utilisation.php
+++ pages/conditions-utilisation.php
@@ -16,8 +16,85 @@
         <main>
            <section>
               <article>
-                   <h1>Conditions d'utilisation</h1>
-                   <h2>Chapô</h2>
+                   <h1>Conditions générales d'utilisation du site droit-image.eu</h1>
+                   
+<h3>ARTICLE 1 : Objet</h3>
+<h>
+Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique des modalités de mise à disposition des services du site droit-image.eu et leur utilisation par « l'Utilisateur ».
+Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur souhaitant accéder au site. Elles constituent le contrat entre le site et l'Utilisateur. L’accès au site par l’Utilisateur signifie son acceptation des présentes conditions générales d’utilisation.
+</h>
+<h3>ARTICLE 2 : Mentions légales</h3>
+<h>
+Ce site est édité par Sophie Candel, Auryane Reisser et Timothé Golla
+Si vous voulez contacter l’un d’eux, merci d’envoyer un courriel sur droit-image@outlook.fr
+Le directeur de la publication est Mme Auryane Reisser
+L'hébergeur du site droit-image.eu est la Société OVH, Société par Actions Simplifiée au capital de 10 000 000 € dont le siège social est situé au 2 Rue Kellermann 59100 ROUBAIX, immatriculée au Registre du Commerce et des Sociétés sous le numéro:
+RCS ROUBAIX-TOURCOING B 424 761 419
+</h>
+<h3>ARTICLE 3 : Définitions</h3>
+<h>
+La présente clause a pour objet de définir les différents termes essentiels du contrat :
+•	Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés par le site.
+•	Contenu utilisateur : ce sont les données transmises par l'Utilisateur au sein du site.
+•	Membre : l'Utilisateur devient membre lorsqu'il est identifié sur le site.
+•	Identifiant et mot de passe : c'est l'ensemble des informations nécessaires à l'identification d'un Utilisateur sur le site. L'identifiant et le mot de passe permettent à l'Utilisateur d'accéder à des services réservés aux membres du site. Le mot de passe est confidentiel.
+</h>
+<h3>ARTICLE 4 : accès aux services</h3>
+<h>
+Le site permet à l'Utilisateur un accès gratuit aux services suivants :
+•	articles d’information sur le droit à l’image en France et en Europe;
+•	publication de commentaires;
+Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.
+L’Utilisateur non membre n'a pas accès aux services réservés aux membres. Pour cela, il doit s'identifier à l'aide de son identifiant et de son mot de passe.
+Le site met en œuvre tous les moyens mis à sa disposition pour assurer un accès de qualité à ses services. L'obligation étant de moyens, le site ne s'engage pas à atteindre ce résultat.
+Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du réseau ou du serveur n'engage pas la responsabilité de droit-image.eu.
+L'accès aux services du site peut à tout moment faire l'objet d'une interruption, d'une suspension, d'une modification sans préavis pour une maintenance ou pour tout autre cas. L'Utilisateur s'oblige à ne réclamer aucune indemnisation suite à l'interruption, à la suspension ou à la modification du présent contrat.
+L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse droit-image@outlook.fr.
+</h>
+<h3>ARTICLE 5 : Propriété intellectuelle</h3>
+<h>
+Les marques, logos, signes et tout autre contenu du site font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.
+L'Utilisateur sollicite l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus.
+L'Utilisateur s'engage à une utilisation des contenus du site dans un cadre strictement privé. Une utilisation des contenus à des fins commerciales est strictement interdite.
+Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.
+Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site. L'Utilisateur ne reçoit aucune justification et notification préalablement à la suppression ou à la modification du contenu Utilisateur.
+</h>
+<h3>ARTICLE 6 : Données personnelles</h3>
+<h>
+Les informations demandées à l’inscription au site sont nécessaires et obligatoires pour la création du compte de l'Utilisateur. En particulier, l'adresse électronique pourra être utilisée par le site pour l'administration, la gestion et l'animation du service.
+Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro [numéro].
+En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+•	son espace personnel ;
+•	un formulaire de contact ;
+•	par mail à droit-image@outlook.fr ;
+</h>
+<h3>ARTICLE 7 : Responsabilité et force majeure</h3>
+<h>
+Les sources des informations diffusées sur le site sont réputées fiables. Toutefois, le site se réserve la faculté d'une non-garantie de la fiabilité des sources. Les informations données sur le site le sont à titre purement informatif. Ainsi, l'Utilisateur assume seul l'entière responsabilité de l'utilisation des informations et contenus du présent site.
+L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite.
+L'Utilisateur assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
+Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+Une garantie optimale de la sécurité et de la confidentialité des données transmises n'est pas assurée par le site. Toutefois, le site s'engage à mettre en œuvre tous les moyens nécessaires afin de garantir au mieux la sécurité et la confidentialité des données.
+La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+</h>
+<h3>ARTICLE 8 : Liens hypertextes</h3>
+<h>
+De nombreux liens hypertextes sortants sont présents sur le site, cependant les pages web où mènent ces liens n'engagent en rien la responsabilité de droit-image.eu qui n'a pas le contrôle de ces liens.
+L'Utilisateur s'interdit donc à engager la responsabilité du site concernant le contenu et les ressources relatives à ces liens hypertextes sortants.
+</h>
+<h3>ARTICLE 9 : Évolution du contrat</h3>
+<h>
+Le site se réserve à tout moment le droit de modifier les clauses stipulées dans le présent contrat.
+</h>
+<h3>ARTICLE 10 : Durée</h3>
+<h>
+La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter de l'utilisation du service.
+</h>
+<h3>ARTICLE 11 : Droit applicable et juridiction compétente</h3>
+<h>
+La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, seuls les tribunaux de la ville de Haguenau sont compétents.
+</h>
+
                </article>
             </section>
               
