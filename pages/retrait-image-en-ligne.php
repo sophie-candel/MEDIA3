19811b329cb950fca9ded9b037431c1e9dd833cf
Unknown <auryane.reisser@gmail.com>
2017-01-09 09:14:06 +0100

    Ajout des derniers textes

--- pages/retrait-image-en-ligne.php
+++ pages/retrait-image-en-ligne.php
@@ -19,7 +19,32 @@
               <article>
                    <h1>Retrait d'une image en ligne</h1>
                    <div class="addthis_inline_share_toolbox"></div>
-                   <h2>Chapô</h2>
+                   <h2>Internet est un multimédia à part entière : on y trouve des informations, du divertissement, des jeux, etc. Mais il peut arriver de trouver quelques surprises. Par exemple, vous pouvez, à votre plus grande surprise, réaliser qu’une photo ou vidéo de vous circule sur les réseaux sociaux. Pourtant vous n’avez nullement donné votre accord pour qu’une telle chose arrive. Pas de panique, vous êtes en droit de demander le retrait de cette image !</h2>
+                  <h3>Deux possibilités :</h3>
+                  <ul>
+                    <li>demander le retrait auprès du responsable du site internet</li>
+                    <li>en parler auprès d’un juge</li>
+                  </ul>
+                  <h3>Deux procédures (elles peuvent être réalisées en parallèle) :</h3>
+                  <ul>
+                    <li>demander la suppression de l’image</li>
+                    <li>demander la dépublication de l’image</li>
+                  </ul>
+                  <h3>Savoir bien distinguer :</h3>
+                  <lu>
+                    <li>la protection des données personnelles : <i>“Toute personne dispose du droit de décider et de contrôler les usages qui sont faits des données à caractère personnel la concernant, dans les conditions fixées par la présente loi.”</i> (article 1, loi informatique et libertés)</li>
+                    <li><a href="droit-image-personnes.php">le droit à l'image</a></li>
+                  </lu>
+                  <h3>procédure à suivre pour le retrait d’une image en ligne</h3>
+                  <ul>
+                    <li>Avant tout, vous devez être certain(e) que vous être identifiable sur ladite photo et/ou vidéo.</li>
+                    <li>Vous devez pouvoir affirmer que vous n’avez pas consenti à apparaître sur la photographie.</li>
+                    <li>Vous pourrez difficilement contester la mise en ligne si : vous avez donné votre accord pour l’exploitation de l’image (par exemple suite à un accord écrit entre le photographe et le sujet) ; la photo a été prise dans un lieu public et vous n’êtes ni isolé, ni reconnaissable.</li>
+                    <li>Vous pouvez contacter l’auteur de la diffusion pour essayer de vous arranger avec lui. Il peut s’agir : du photographe / vidéaste ; de l’organisme utilisant ces images pour un but commercial, publicitaire (cadre professionnel).</li>
+                    <li>Vous pouvez contacter le responsable du site internet où la photo est publiée.
+Usez de tous les moyens possibles : par le site lui-même, par les réseaux sociaux ; en indiquant un motif légitime et en mettant en pièce-jointe un justificatif d’identité (il doit comporter votre signature). </li>
+                  </ul>
+                  <p>En cas de non respect de la loi, sachez que des sanctions sont prévues à cet effet (jusqu’à 1 an d’emprisonnement et jusqu’à 45 000 € d’amende).</p>
                </article>
             </section>
         </main>
