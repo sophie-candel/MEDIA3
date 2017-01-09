d4d9468341b5742d3cdca301496b6bfe985c1673
sjcandel <sophiee.candel@free.fr>
2017-01-09 22:24:31 +0100

    test compte à rebours

--- includes/notation.php
+++ includes/notation.php
@@ -26,3 +26,7 @@
     <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
 </article>
 </section>
+
+
+
+
