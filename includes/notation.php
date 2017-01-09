cd5a0417d5d455ab7122536c39fe22abb4c2b9de
Unknown <sophie.candel@etu.unistra.fr>
2017-01-09 09:34:14 +0100

    microdonnées commentaires

--- includes/notation.php
+++ includes/notation.php
@@ -1,17 +1,22 @@
 <section class="notation">
-                    <h3>Laisser un avis</h3>
-                    <article>
-                        <h4>Noter l'article</h4>                  
-                        
-                       <div class="rating"><!--
-                            --><a href="#5" title="Donner 5 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-                            --><a href="#4" title="Donner 4 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-                            --><a href="#3" title="Donner 3 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-                            --><a href="#2" title="Donner 2 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-                            --><a href="#1" title="Donner 1 étoile"><i class="fa fa-star" aria-hidden="true"></i></a>
-                        </div>
-                  </article>
-                  <article>
-                        <h4>Laisser un commentaire</h4>
-                  </article>
-            </section>
+    <h3>Commentaires</h3>
+    <ul>
+        <li>Nombre de commentaires : <span  itemprop="commentCount">2</span></li>
+        <li>Meilleurs note : </li>
+    </ul>
+    
+    <article>                 
+        <div class="rating"><!--
+        --><a href="#5" title="5 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#4" title="4 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#3" title="3 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#2" title="2 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#1" title="1 étoile"><i class="fa fa-star" aria-hidden="true"></i></a>
+        </div>
+    </article>
+    
+    <article>
+        <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
+        <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
+    </article>
+</section>
--- pages/mentions-legales.php
+++ pages/mentions-legales.php
@@ -1,49 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <!--HEADER-->
-    <head>
-        <meta name="description" content="Informations concernant les condition sd'utilisation" />
-        <title>Droit à l'image - conditions d'utilisation</title>
-        <?php include("../includes/header.php"); ?>
-    </head>
-    
-    <body class="texte_fonce">
-        <!--NAV-->
-        <?php include("../includes/nav.php"); ?>
+<!--HEADER-->
 
+<head>
+    <meta name="description" content="Informations concernant les conditions d'utilisation" />
+    <title>Droit à l'image - mentions légales</title>
+    <?php include("../includes/header.php"); ?>
+</head>
 
+<body class="texte_fonce">
+    <!--NAV-->
+    <?php include("../includes/nav.php"); ?>
         <!--MAIN-->
         <main>
-           <section>
-              <article>
-                   <h1>Conditions générales d'utilisation du site droit-image.eu</h1>
-                   
+            <section>
+                <article>
+                    <h1>Mentions légales du site droit-image.eu</h1>
                     <h3>Le site droit-image.eu est édité par:</h3>
-                    <p>Ce site est édité par Sophie Candel, Auryane Reisser et Timothé Golla
-                    Si vous voulez contacter l’un d’eux, merci d’envoyer un courriel sur droit-image@outlook.fr</p>
+                    <p>Ce site est édité par Sophie Candel, Auryane Reisser et Timothé Golla Si vous voulez contacter l’un d’eux, merci d’envoyer un courriel sur droit-image@outlook.fr</p>
                     <p>Le directeur de la publication est Mme Auryane Reisser</p>
-
- 
-                  <h3>Le site droit-image.eu est hébergé par: </h3>
-                  <p>OVH,</p>
-                  <p>Société par Actions Simplifiée,</p>
-                    <p>au capital de 10 000 000 euros, dont le siège social est établi
-                    2 Rue Kellermann 59100 ROUBAIX</p>
-                  <p>immatriculée au Registre du Commerce et des Sociétés sous le numéro:RCS ROUBAIX-TOURCOING B 424 761 419</p>
- 
-                  <h3>Le directeur de la publication :</h3>
-                  <p>Le directeur de la publication est Auryane Reisser</p>
-                  <p>Les commentaires ne sont pas modérés avant publication. Si vous trouvez l’un de ces commentaires offensant, vous pouvez demander son retrait à l'adresse suivante : droit-image@outlook.fr</p>
-
-               </article>
+                    <h3>Le site droit-image.eu est hébergé par: </h3>
+                    <p>OVH,</p>
+                    <p>Société par Actions Simplifiée,</p>
+                    <p>au capital de 10 000 000 euros, dont le siège social est établi 2 Rue Kellermann 59100 ROUBAIX</p>
+                    <p>immatriculée au Registre du Commerce et des Sociétés sous le numéro:RCS ROUBAIX-TOURCOING B 424 761 419</p>
+                    <h3>Le directeur de la publication :</h3>
+                    <p>Le directeur de la publication est Auryane Reisser</p>
+                    <p>Les commentaires ne sont pas modérés avant publication. Si vous trouvez l’un de ces commentaires offensant, vous pouvez demander son retrait à l'adresse suivante : droit-image@outlook.fr</p>
+                </article>
             </section>
-              
-            
-            
         </main>
-
-
         <!--FOOTER-->
         <?php include("../includes/footer.php"); ?>
-    </body>
-</html>        
+</body>
+
+</html>
