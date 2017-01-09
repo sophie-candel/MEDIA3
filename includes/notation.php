bfedc55c87bf2f18b06f2c06d09c142abead82f1
Unknown <sophie.candel@etu.unistra.fr>
2017-01-09 10:28:23 +0100

    microdonnées commentaire suite

--- includes/notation.php
+++ includes/notation.php
@@ -1,27 +1,27 @@
 
     <h3>Commentaires</h3>
     <ul>
-        <li>Nombre de commentaires : <span  itemprop="commentCount">2</span></li>
-        <li>Meilleurs note : </li>
+        <li itemprop="">Nombre de commentaires : 2</li>
+        <li itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">Meilleure note : <span itemprop="bestRating">4</span><meta itemprop="ratingCount" content="2"/></li>
     </ul>
     <article>
-        <div class="rating"><!--
-        --><a href="#5" title="Donner 5 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#4" title="Donner 4 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#3" title="Donner 3 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#2" title="Donner 2 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#1" title="Donner 1 étoile"><i class="fa fa-star" aria-hidden="true"></i></a>
+        <div class="rating" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"><!--
+        --><a href="#5" title="Donner 5 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#4" title="Donner 4 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#3" title="Donner 3 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#2" title="Donner 2 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#1" title="Donner 1 étoile" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a>
         </div>
         <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
      </article>
     
      <article>
-        <div class="rating"><!--
-        --><a href="#5" title="Donner 5 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#4" title="Donner 4 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#3" title="Donner 3 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#2" title="Donner 2 étoiles"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-        --><a href="#1" title="Donner 1 étoile"><i class="fa fa-star" aria-hidden="true"></i></a>
+        <div class="rating" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"><!--
+        --><a href="#5" title="Donner 5 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#4" title="Donner 4 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#3" title="Donner 3 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#2" title="Donner 2 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
+        --><a href="#1" title="Donner 1 étoile" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a>
         </div>
         <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
     </article>
