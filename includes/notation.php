2013d3fd0721111795424b861f72e1c6b691327a
Unknown <sophie.candel@etu.unistra.fr>
2017-01-10 14:53:36 +0100

    microdonnées include "notation" THE END II

--- includes/notation.php
+++ includes/notation.php
@@ -1,27 +1,28 @@
 
     <h3>Commentaires</h3>
-    <ul>
-        <li itemprop="">Nombre de commentaires : 2</li>
-        <li itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">Meilleure note : <span itemprop="bestRating">4</span><meta itemprop="ratingCount" content="2"/></li>
-    </ul>
-    <article>
+
+    <article itemscope itemtype="https://schema.org/Comment">
         <div class="rating" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"><!--
         --><a href="#5" title="Donner 5 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#4" title="Donner 4 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#3" title="Donner 3 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#2" title="Donner 2 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#1" title="Donner 1 étoile" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a>
+            <meta itemprop="ratingCount" content="2"/>
+            <meta itemprop="bestRating" content="4.2"/>
         </div>
-        <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
+        <p class="comment" itemprop="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
      </article>
     
-     <article>
+    <article itemscope itemtype="https://schema.org/Comment">
         <div class="rating" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"><!--
         --><a href="#5" title="Donner 5 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#4" title="Donner 4 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#3" title="Donner 3 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#2" title="Donner 2 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
         --><a href="#1" title="Donner 1 étoile" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a>
+            <meta itemprop="ratingCount" content="2"/>
+            <meta itemprop="bestRating" content="4.2"/>
         </div>
-        <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
-    </article>
+        <p class="comment" itemprop="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
+     </article>
