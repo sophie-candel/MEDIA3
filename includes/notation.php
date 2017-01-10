c0551c0836564e2c1087e38a41c904e8d5dcbe57
Unknown <sophie.candel@etu.unistra.fr>
2017-01-10 15:06:25 +0100

    euphorie

--- includes/notation.php
+++ includes/notation.php
@@ -1,22 +1,9 @@
-<section class="notation"> 
-<h3>Commentaires</h3>
-<ul>
-    <li itemprop="">Nombre de commentaires : 2</li>
-    <li itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">Meilleure note : <span itemprop="bestRating">4</span><meta itemprop="ratingCount" content="2"/></li>
-</ul>
-<SCRIPT charset="utf-8" type="text/javascript" src="https://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fdroitimageeu-21%2F8010%2Ff71edb2e-d10f-4629-bd0b-a8d8d1fe8a76&Operation=GetScriptTemplate"> </SCRIPT> <NOSCRIPT><A rel="nofollow" HREF="https://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fdroitimageeu-21%2F8010%2Ff71edb2e-d10f-4629-bd0b-a8d8d1fe8a76&Operation=NoScript">Widgets Amazon.fr</A></NOSCRIPT>
-<article>
-    <div class="rating" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"><!--
-    --><a href="#5" title="Donner 5 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-    --><a href="#4" title="Donner 4 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-    --><a href="#3" title="Donner 3 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-    --><a href="#2" title="Donner 2 étoiles" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a><!--
-    --><a href="#1" title="Donner 1 étoile" itemprop="ratingValue"><i class="fa fa-star" aria-hidden="true"></i></a>
-    </div>
-    <p class="comment" itemprop="comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel varius mauris, vel suscipit metus. Sed a porttitor metus. Quisque laoreet molestie est quis blandit. Mauris vel condimentum ante. Integer purus ligula, suscipit quis velit et, interdum tincidunt purus. Praesent accumsan est eget lacinia fringilla. Morbi sit amet tellus malesuada, scelerisque dui ac, lobortis turpis. Nulla pellentesque, lectus et dapibus rhoncus, nunc libero finibus dui, in interdum risus massa id arcu.</p>
- </article>
 
-    <h3>Commentaires</h3>
+<h3>Commentaires</h3>
+
+    <article>
+        <SCRIPT charset="utf-8" type="text/javascript" src="https://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fdroitimageeu-21%2F8010%2Ff71edb2e-d10f-4629-bd0b-a8d8d1fe8a76&Operation=GetScriptTemplate"> </SCRIPT> <NOSCRIPT><A rel="nofollow" HREF="https://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fdroitimageeu-21%2F8010%2Ff71edb2e-d10f-4629-bd0b-a8d8d1fe8a76&Operation=NoScript">Widgets Amazon.fr</A></NOSCRIPT>
+    </article>
 
     <article itemscope itemtype="https://schema.org/Comment">
         <div class="rating" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"><!--
